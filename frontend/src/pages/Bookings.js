35b3e4808e96e882b0a749a2743ad665cb61b4ab
romanloky <34403942+romanloky@users.noreply.github.com>
2019-05-08 02:01:50 +0530

    Update Bookings.js

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -38,7 +38,7 @@ class BookingsPage extends Component {
         `
     };
 
-    fetch('http://localhost:8000/graphql', {
+    fetch('https://romanloky.herokuapp.com:4000/graphql', {
       method: 'POST',
       body: JSON.stringify(requestBody),
       headers: {
